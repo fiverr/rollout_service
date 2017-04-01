11eecfef7cbe23b2478dd41f56ca2330f41a08d1
Yossi Eynav <yossi.eynav@gmail.com>
2017-04-01 15:57:58 +0300

    [FIX] - Fix restful Response model

--- restful_models/response.rb
+++ restful_models/response.rb
@@ -1,6 +1,6 @@
 module RestfulModels
    class Response < Grape::Entity
       expose :data, unless: Proc.new {|field| field.blank?}
-      expose :message, if: ->(response) { response[:data].blank? }
+      expose :message, if: ->(response, _) { response[:data].blank? }
    end
 end
